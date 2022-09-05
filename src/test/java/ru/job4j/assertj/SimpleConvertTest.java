dac1866af03c6339731e71a61199f5e5a740d371
Kirill <info@sampx.ru>
2022-09-05 22:17:45 +0300

    Утверждения с коллекциями [#504885 #339622]

--- src/test/java/ru/job4j/assertj/SimpleConvertTest.java
+++ src/test/java/ru/job4j/assertj/SimpleConvertTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.data.Index;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,5 +31,28 @@ class SimpleConvertTest {
                     assertThat(e.length() < 10);
                     assertThat(e.startsWith("first"));
                 });
+    }
+
+    @Test
+    void checkToSet() {
+        SimpleConvert conv = new SimpleConvert();
+        Set<String> array = conv.toSet("First", "Second", "Three", "Four");
+        assertThat(array)
+                .isNotEmpty()
+                .isNotNull()
+                .contains("Three")
+                .hasSize(4)
+                .filteredOn(e -> e.length() <= 5
+                        && e.startsWith("F"))
+                .hasSize(2);
+    }
+
+    @Test
+    void checkToMap() {
+        SimpleConvert conv = new SimpleConvert();
+        Map<Integer, String> map = Map.of(1, "first", 2, "second",
+                                          3, "Three", 4, "four");
+        assertThat(map).doesNotContainKey(0)
+                .containsEntry(1, "1");
     }
 }
