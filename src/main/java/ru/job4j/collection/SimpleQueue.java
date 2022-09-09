7702bd1cad147913ec3453531e068d251ade363e
crash <info@sampx.ru>
2022-09-09 10:52:51 +0300

    Утверждения с исключениями [#504886]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -3,14 +3,39 @@ package ru.job4j.collection;
 import java.util.NoSuchElementException;
 
 public class SimpleQueue<T> {
+    private int inSize = 0;
+    private int outSize = 0;
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
 
     public T poll() {
-        return out.pop();
+        isEmpty();
+        T temp = null;
+        if (inSize > 0) {
+            inSize--;
+            temp = in.pop();
+        } else {
+            for (int i = 0; i < outSize; i++) {
+                in.push(out.pop());
+                inSize++;
+            }
+            poll();
+        }
+
+
+        return temp;
     }
 
     public void push(T value) {
         in.push(value);
+        inSize++;
     }
+
+    private void isEmpty() {
+        if (inSize == 0 && outSize == 0) {
+            throw new NoSuchElementException();
+        }
+    }
+
+
 }
