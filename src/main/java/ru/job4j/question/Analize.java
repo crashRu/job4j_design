8d9d7000496d5bd8c75a78ce4ae994c3ed64e772
Kirill <info@sampx.ru>
2022-11-11 19:32:07 +0300

    1. Создать элементарную структуру дерева [#1711]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,18 @@
+package ru.job4j.question;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Analize {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        HashSet<User> prevMap = new HashSet<>(previous);
+        return new Info(1,2,3);
+    }
+
+    public static boolean removeUser(User user) {
+
+        return true;
+    }
+
+}
