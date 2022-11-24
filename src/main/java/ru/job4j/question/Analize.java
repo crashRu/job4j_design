d45a4df0055424278346aa33b4bcad1546987270
Kirill <info@sampx.ru>
2022-11-24 17:48:50 +0300

    1. Создать элементарную структуру дерева [#1711]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -2,6 +2,7 @@ package ru.job4j.question;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Analize {
 
@@ -9,6 +10,10 @@ public class Analize {
         int addUserInfo = 0;
         int removeUserInfo = 0;
         int changeUserInfo;
+
+        changeUserInfo = previous.stream()
+                .collect(Collectors.toMap(prev -> prev.getId(), prev -> prev.getName()))
+                .containsKey(current.contains())
 
         for (User user : current) {
             if (!previous.contains(user)) {
