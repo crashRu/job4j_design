9839cd59488fa94d26ad564c98aacc6e3cc916a9
Kirill <info@sampx.ru>
2022-11-25 12:59:02 +0300

    1. Создать элементарную структуру дерева [#1711]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -2,39 +2,25 @@ package ru.job4j.question;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
-        int addUserCount = 0;
-        int changeUserCount = 0;
-        int removeUserCount = 0;
-
         Map<Integer, String> currMap = current.stream()
                 .collect(Collectors.toMap(User::getId, User::getName));
-
         Map<Integer, String> prevMap = previous.stream()
                 .collect(Collectors.toMap(User::getId, User::getName));
+        return new Info(elementCount(x -> !prevMap.containsKey(x.getId()), current),
+                elementCount(x -> currMap.containsKey(x.getId())
+                        && !(x.getName().equals(currMap.get(x.getId()))), previous),
+                elementCount(x -> !currMap.containsKey(x.getId()), previous));
+    }
 
-        for (User prev : previous) {
-            if (currMap.containsKey(prev.getId())
-                    && !prev.getName().equals(currMap.get(prev.getId()))) {
-                changeUserCount++;
-            }
-        }
-
-        for (User user : current) {
-            if (!prevMap.containsKey(user.getId())) {
-                addUserCount++;
-            }
-        }
-
-        for (User user : previous) {
-            if (!currMap.containsKey(user.getId())) {
-                removeUserCount++;
-            }
-        }
-        return new Info(addUserCount, changeUserCount, removeUserCount);
+    public static int elementCount(Predicate<? super User> pred, Set<User> set) {
+        return (int) set.stream()
+                .filter(pred)
+                .count();
     }
 }
--- src/main/java/ru/job4j/question/Info.java
+++ src/main/java/ru/job4j/question/Info.java
@@ -12,9 +12,8 @@ public class Info {
         this.added = added;
         this.changed = changed;
         this.deleted = deleted;
+
     }
-
-
 
     public int getAdded() {
         return added;
@@ -42,12 +41,8 @@ public class Info {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Info info = (Info) o;
         return added == info.added && changed == info.changed && deleted == info.deleted;
     }
