b21a1114b8be20250149cbab3156adf2e776db24
crash_ru <info@sampx.ru>
2022-11-27 20:10:18 +0300

    Реализовать собственную структуру данных - HashMap [#1008 #353394]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -1,26 +1,46 @@
 package ru.job4j.io;
 
+import javax.sound.midi.Patch;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("even.txt")) {
-            byte[] buffer = new byte[in.available()];
+       parseByte(readFile("even.txt")).forEach(System.out::println);
+    }
+
+    public static byte[] readFile(String patch) {
+        byte[] buffer = null;
+        try (FileInputStream in = new FileInputStream(patch)) {
+            buffer = new byte[in.available()];
             in.read(buffer, 0, in.available());
-            String a = "";
-            for (int i = 0; i < buffer.length; i++) {
-                 a = a + (char) buffer[i];
-            }
-            String[] b = a.split("\n");
-            for (String test : b) {
-                System.out.println(test);
-            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return buffer;
+    }
+
+    public static List<Integer> parseByte(byte[] bytesList) {
+        String result = "";
+        for (int index = 0; index < bytesList.length; index++) {
+            result = result + (char) bytesList[index];
+        }
+        String[] a = result.split("\n");
+        ArrayList list = new ArrayList();
+        for (String b : a) {
+            System.out.println(a.toString());
+          list.add(Integer.parseInt(b));
+            System.out.println(b);
+        }
+
+        return new ArrayList<>();
     }
 }
