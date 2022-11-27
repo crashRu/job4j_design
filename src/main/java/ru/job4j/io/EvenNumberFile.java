249ae8b0b7a007c34e8d5fe2c8dc508898d4e231
Kirill <info@sampx.ru>
2022-11-27 18:11:08 +0300

    0.2. FileInputStream [#4898]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -7,9 +7,15 @@ import java.io.IOException;
 public class EvenNumberFile {
     public static void main(String[] args) {
         try (FileInputStream in = new FileInputStream("even.txt")) {
-            int read;
-            while ((read = in.read()) != -1) {
-                System.out.print((char) read);
+            byte[] buffer = new byte[in.available()];
+            in.read(buffer, 0, in.available());
+            String a = "";
+            for (int i = 0; i < buffer.length; i++) {
+                 a = a + (char) buffer[i];
+            }
+            String[] b = a.split("\n");
+            for (String test : b) {
+                System.out.println(test);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
