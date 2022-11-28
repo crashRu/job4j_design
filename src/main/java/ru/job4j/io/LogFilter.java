81fe28b7b577deb19c3e0658eeb64788c9c881ac
Kirill <info@sampx.ru>
2022-11-29 00:09:24 +0300

    0.3. BufferedReader. [#25248]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -9,7 +9,7 @@ public class LogFilter {
     public List<String> filter(String file) {
         List<String> list = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            in.lines().limit(5).forEach(x -> list.add(x));
+            in.lines().forEach(x -> list.add(x));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -19,7 +19,6 @@ public class LogFilter {
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
-        log.stream().forEach(System.out::println);
-
+        System.out.println(log);
     }
 }
