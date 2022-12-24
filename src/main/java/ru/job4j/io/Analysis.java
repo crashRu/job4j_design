f38f7671837602e8df727e6648fa45b5ba6663aa
Kirill <info@sampx.ru>
2022-12-24 19:38:19 +0300

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,61 +1,35 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class Analysis {
-    private static List<DataStore> list = new LinkedList<>();
-    private static StringBuilder builder = new StringBuilder();
 
     public static void unavailable(String source, String target) {
+        StringBuilder builder = new StringBuilder();
+
         try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
-            list = reader.lines()
+            reader.lines()
                     .filter(line -> !line.isEmpty())
-                    .map(line -> new DataStore(line.substring(0, line.indexOf("\s")),
-                            line.substring(line.indexOf("\s"))))
+                    .map(line -> {
+                        int subStringIndex = Integer.parseInt(line.substring(0, line.indexOf("\s")));
+                        String subStringDate = line.substring(line.indexOf("\s"));
+                        if (builder.length() % 2 != 0 && subStringIndex >= 400) {
+                            builder.append(subStringDate + ";");
+                        } else if (builder.length() % 2 == 0 && subStringIndex < 400) {
+                            builder.append(subStringDate + ";");
+                        }
+                        return builder;
+                    })
                     .collect(Collectors.toList());
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-        healthHandler();
-        writingFile(target);
-    }
-
-    public static void writingFile(String patch) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(patch))) {
-            writer.write(builder.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void healthHandler() {
-        boolean conditionServer = true;
-        for (DataStore dat : list) {
-            if (conditionServer && dat.code >= 400) {
-                builder.append(dat.data + ";");
-                conditionServer = false;
-            } else if (!conditionServer && dat.code < 400) {
-                builder.append(dat.data + ";" + System.lineSeparator());
-                conditionServer = true;
-            }
-        }
+        System.out.println(builder);
     }
 
     public static void main(String[] args) {
         unavailable("unavailable.csv", "serverLog.csv");
-    }
-}
-
-class DataStore {
-    int code;
-    String data;
-
-    public DataStore(String code, String data) {
-        this.code = Integer.parseInt(code);
-        this.data = data;
     }
 }
