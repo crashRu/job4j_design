671d3741a3b40dc21c8af014d3cbb1c35ff7fc21
Crash_ru <info@sampx.ru>
2023-01-04 20:47:01 +0300

    4.0. File [#252491]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -8,14 +8,23 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private static List<FileProperty> fileList = new ArrayList<>();
+    private static Map<FileProperty, List<Path>> fileList = new HashMap<>();
+
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        fileList.add(new FileProperty(file.toFile().length(), file.getFileName().toString()));
+        FileProperty tempKey = new FileProperty(file.toFile().length(), file.getFileName().toString());
+        if (!fileList.containsKey(tempKey)) {
+            fileList.put(tempKey, new ArrayList<>());
+            fileList.get(tempKey).add(file);
+        } else {
+            fileList.get(tempKey).add(file);
+        }
         return super.visitFile(file, attrs);
     }
 
     public static void printDuplicate() {
-        fileList.stream().filter(p -> !fileList.contains(p)).forEach(System.out::println);
+        fileList.keySet().stream()
+                .filter(p -> fileList.get(p).size() > 1)
+                .forEach(p -> System.out.println(fileList.get(p)));
     }
 }
