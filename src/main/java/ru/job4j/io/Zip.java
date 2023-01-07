fe3755a7d6936ae1241c0e4c2aaf7d041c5acf1c
Kirill <info@sampx.ru>
2023-01-07 12:33:22 +0300

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -8,8 +8,21 @@ import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public void packFiles(List<Path> sources, File target) {
+    private Path pathZip;
 
+    public void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path path : sources) {
+                zip.putNextEntry(new ZipEntry(path.toString()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toFile()))) {
+                    zip.write(out.readAllBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void packSingleFile(File source, File target) {
@@ -18,16 +31,24 @@ public class Zip {
             try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
                 zip.write(out.readAllBytes());
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    public List<Path> pathList(String[] args) throws IOException {
+        ArgsName.of(args);
+        ArgsName arguments = ArgsName.of(args);
+        Path directory = Path.of(arguments.get("d"));
+        this.pathZip = Path.of(arguments.get("o"));
+        return Search.search(directory, p -> !p.toString().endsWith(arguments.get("e")));
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Not enough arguments");
+        }
+        ArgsName.of(args);
         Zip zip = new Zip();
-        zip.packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
-        );
+        zip.packFiles(zip.pathList(args), zip.pathZip.toFile());
     }
 }
