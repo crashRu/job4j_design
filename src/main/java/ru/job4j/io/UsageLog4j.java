83d556a346fc9b61f02c309b467543b04df75e8a
Crash_ru <info@sampx.ru>
2023-01-19 21:14:21 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,16 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        int age = 25;
+        char feMail = 'm';
+        double amount = 346.2;
+        long ram = 68903;
+        boolean smart = false;
+        byte children = 2;
+        short shortTest = 12;
+        float floatTest = 11.1F;
+
+        LOG.warn("age: {}\nfeMail: {}\namount: {}\nram: {}\nsmart: {}\nchildren {}\nshortTest: {}\nfloatTest: {}",
+                age, feMail, amount, ram, smart, children, shortTest, floatTest);
     }
 }
