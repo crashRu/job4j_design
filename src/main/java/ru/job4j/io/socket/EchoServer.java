7ef59d95ae879021f9b31a1bec4f3336b34874f3
Crash_ru <info@sampx.ru>
2023-01-20 21:46:29 +0300

    4. Slf4j - вывод exception. [#268853 #383020]

--- src/main/java/ru/job4j/io/socket/EchoServer.java
+++ src/main/java/ru/job4j/io/socket/EchoServer.java
@@ -6,10 +6,9 @@ import java.net.Socket;
 import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.job4j.io.UsageLog4j;
 
 public class EchoServer {
-    private static final Logger LOG_ECHO_SERVER = LoggerFactory.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger("Echo_Server");
 
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
@@ -31,11 +30,11 @@ public class EchoServer {
                     }
                     out.flush();
                 } catch (IOException exception) {
-                    LOG_ECHO_SERVER.error("Exception in log example", exception);
+                    LOG.error("Exception in log example", exception);
                 }
             }
         } catch (IOException exception) {
-            LOG_ECHO_SERVER.error("Exception in log example", exception);
+            LOG.error("Exception in log example", exception);
         }
     }
 }
