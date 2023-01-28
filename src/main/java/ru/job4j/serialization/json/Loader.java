a9c8f8ddb85d42d497f867dbbf723c0d63a047df
Crash_ru <info@sampx.ru>
2023-01-28 16:14:23 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/serialization/json/Loader.java
+++ src/main/java/ru/job4j/serialization/json/Loader.java
@@ -1,27 +1,10 @@
 package ru.job4j.serialization.json;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import java.io.StringReader;
-import java.io.StringWriter;
+import org.json.JSONObject;
 
 public class Loader {
     public static void main(String[] args) throws Exception {
         Worker worker = new Worker(new String[]{"Достоинство 1", "Достоинство 2"}, Position.DIRECTOR, "Jon", 20, true);
-        JAXBContext context = JAXBContext.newInstance(Worker.class);
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        String xml = "";
-        try (StringWriter writer = new StringWriter()) {
-            marshaller.marshal(worker, writer);
-            xml = writer.getBuffer().toString();
-            System.out.println(xml);
-        }
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-        try (StringReader reader = new StringReader(xml)) {
-            Worker result = (Worker) unmarshaller.unmarshal(reader);
-            System.out.println(result);
-        }
+        System.out.println(new JSONObject(worker));
     }
 }
--- src/main/java/ru/job4j/serialization/json/Main.java
+++ src/main/java/ru/job4j/serialization/json/Main.java
@@ -1,31 +1,27 @@
 package ru.job4j.serialization.json;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import ru.job4j.serialization.Contact;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        final Person person = new Person(false, 30, new Contact(99, "11-111"),
-                new String[] {"Worker", "Married"});
-
-        /* Преобразуем объект person в json-строку. */
-        final Gson gson = new GsonBuilder().create();
-        System.out.println(gson.toJson(person));
-
-        /* Модифицируем json-строку */
-        final String personJson =
-                "{"
-                        + "\"sex\":false,"
-                        + "\"age\":35,"
-                        + "\"contact\":"
-                        + "{"
-                        + "\"phone\":\"+7(924)111-111-11-11\""
-                        + "},"
-                        + "\"statuses\":"
-                        + "[\"Student\",\"Free\"]"
-                        + "}";
-        final Person personMod = gson.fromJson(personJson, Person.class);
-        System.out.println(personMod);
+        JSONObject jsonContact = new JSONObject("{\"phone\":\"+7(924)111-111-11-11\"}");
+        List<String> list = new ArrayList<>();
+        list.add("Student");
+        list.add("Free");
+        JSONArray jsonStatuses = new JSONArray(list);
+        final Person person = new Person(false, 30, new Contact(111111, "999999999"), new String[]{"Worker", "Married"});
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("sex", person.isSex());
+        jsonObject.put("age", person.getAge());
+        jsonObject.put("contact", jsonContact);
+        jsonObject.put("statuses", jsonStatuses);
+        System.out.println(jsonObject);
+        System.out.println(new JSONObject(person).toString());
     }
 }
