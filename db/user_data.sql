8310d0c6b1fbd1fe169ce7e6494a12f18a8d4227
Kirill <info@sampx.ru>
2023-02-26 21:39:06 +0300

    3. Поиск файлов по критерию [#783 #387665]

--- db/user_data.sql
+++ db/user_data.sql
@@ -7,4 +7,7 @@ create table user_data (
 );
 insert into user_data(name, password, age, salary) values ('Kirill','passwd','27','2000');
 insert into user_data(name, password, age, salary) values ('Вася','passwd','37','6000');
+update user_data set age = 39 WHERE age > 30;
+select * from user_data; 
+delete from user_data;
 select * from user_data;
