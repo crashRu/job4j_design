85aa58a87e8a3ac015d2cf79da63a8dc5bb0a486
Kirill <info@sampx.ru>
2025-03-13 03:07:18 +0300

    0.2. FileInputStream [#4898]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -1,44 +1,23 @@
 package ru.job4j.io;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
-        parseByte(readFile("even.txt"))
-                .forEach(number -> System.out.println(number + " " + (number % 2 == 0)));
-    }
-
-    public static byte[] readFile(String patch) {
-        byte[] buffer = null;
-        try (FileInputStream in = new FileInputStream(patch)) {
-            buffer = new byte[in.available()];
-            in.read(buffer, 0, in.available());
-        } catch (FileNotFoundException e) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            Arrays.stream(text.toString().split("\n"))
+                    .map(x -> Integer.parseInt(x))
+                    .forEach(a -> System.out.println(a + " " + (a % 2 == 0)));
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return buffer;
-    }
-
-    public static List<Integer> parseByte(byte[] bytesList) {
-        String result = "";
-        for (int index = 0; index < bytesList.length; index++) {
-            result = result + (char) bytesList[index];
-        }
-        try {
-            return Arrays.stream(result.split("\n"))
-                    .map(line -> Integer.parseInt(line))
-                    .collect(Collectors.toList());
-        } catch (NumberFormatException exception) {
-            exception.printStackTrace();
-            return new ArrayList<>();
         }
     }
 }
+
+
